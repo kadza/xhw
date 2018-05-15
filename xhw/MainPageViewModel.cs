f2ea7b6430d1e9781af213eb21df99aa9863dff3
KUJAWIAK Lukasz <lkujawia@lod-mlkujawia01.ingrnet.com>
2018-05-15 12:22:01 +0200

    Get platform mock works fine

---
+++ xhw/MainPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace xhw
+{
+	public class MainPageViewModel : INotifyPropertyChanged
+    {
+		public MainPageViewModel()
+		{
+			GetPlatform = new Command(GetPlatformImpl);
+		}
+       
+		string platform = "test";
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string Platform
+		{
+			get {
+				return platform;
+			}
+
+			set {
+				platform = value;
+				OnPropertyChanged("Platform");
+			}
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+		public ICommand GetPlatform { get; }
+        
+		private void GetPlatformImpl(){
+			Platform = "test1";
+		}
+        
+	}
+}
